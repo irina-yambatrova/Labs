6daac940f760e12391e8a87763d904b609daf890
irina-yambatrova <yambatrova.irina@gmail.com>
2015-12-09 15:55:51 +0300

    no message

--- 1.1.py
+++ 1.1.py
@@ -1,17 +1,17 @@
-import urllib2 #чтение кода страницы
-import re      #регулярные выражения
-import urllib #скачивание
+import urllib2 
+import re      
+import urllib 
 
 extensions = ['gif', 'png', 'bmp', 'jpg', 'jpeg', 'png']
 k = 1
 url =  'http://lenta.ru/'
 image_urls = re.findall('img .*?src="(.*?)"', urllib2.urlopen(url).read()) 
-for i in range(len(image_urls)): #поиск файлов, которые нужно скачивать
+for i in range(len(image_urls)): 
     address = image_urls[i]
     if address[address.rfind('.') + 1 : ] in extensions:
         image_name = address[address.rfind('/') + 1 : ]
         if '%' in image_name:
             image_name = k
             k += 1
-        urllib.urlretrieve(address, image_name) #cамо скачивание
+        urllib.urlretrieve(address, image_name) 
 print 'end'
